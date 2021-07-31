fee260c044f889c1f74877cbef2a3400c0f227dd
Christopher Lyon <christopherlyon.cl@gmail.com>
2021-07-31 22:31:43 +0200

    Added Hops and Beer.
    Added glasses and raw materials as ITEMS.
    
    Co-Authored-By: Aksel Trøan <49494113+AkselTroan@users.noreply.github.com>

--- src/main/java/beermod/beer/mod/BeerMod.java
+++ src/main/java/beermod/beer/mod/BeerMod.java
@@ -1,14 +1,28 @@
 package beermod.beer.mod;
 
+
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 
 public class BeerMod implements ModInitializer {
+     
+	// an instance of our new item
+	public static final Item BEER = new Item(new FabricItemSettings().group(ItemGroup.FOOD));
+	public static final Item HOPS = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
+	public static final Item BEERGLASS = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
+
+ 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		Registry.register(Registry.ITEM, new Identifier("beermod", "beer"), BEER); // LOWER CASE NAME
+		Registry.register(Registry.ITEM, new Identifier("beermod", "hops"), HOPS); // LOWER CASE NAME
+		Registry.register(Registry.ITEM, new Identifier("beermod", "beerglass"), BEERGLASS); // LOWER CASE NAME
 
-		System.out.println("Hello Fabric world!");
-	}
+	} 
 }
+
