dd03ae4921dc2f54c846ed00e1771b14794ccba3
Christopher Lyon <christopherlyon.cl@gmail.com>
2021-07-31 21:07:44 +0200

    First Launch

--- src/main/java/beermod/beer/mod/BeerMod.java
+++ src/main/java/beermod/beer/mod/BeerMod.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package beermod.beer.mod;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class BeerMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
